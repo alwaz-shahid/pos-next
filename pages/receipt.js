ea911291b1610c47cef3a553b530b2701ddbbb39
Alwaz18 <alwazk8@gmail.com>
2022-03-30 21:02:40 +0500

    d

--- pages/receipt.js
+++ pages/receipt.js
@@ -1,10 +1,7 @@
 import React from "react";
 import Receipt from "../components/Receipt";
-import useStore from "../store";
 
 const receipt = () => {
-  const orders = useStore((state) => state.orders);
-  const resetCart = useStore((state) => state.resetCart);
   return (
     <div className="flex flex-col justify-start mx-auto items-center ">
       <h1 className="text-xl font-semibold  pb-2 text-center">Picante PK</h1>
