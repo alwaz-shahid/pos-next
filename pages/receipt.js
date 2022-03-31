25e2c675cc536de7ed46bf52e1f5aa7a1db7a9bc
Alwaz18 <alwazk8@gmail.com>
2022-03-31 19:39:57 +0500

    add

--- pages/receipt.js
+++ pages/receipt.js
@@ -6,14 +6,13 @@ const receipt = () => {
   return (
     <div className="flex flex-col justify-start py-2 bg-white mx-auto items-center ">
       <h1 className="text-2xl italic pt-1 pb-2"> PICANTE PK</h1>
+      <p className="p-2 italic">TRAVEL THROUGH TASTE</p>
       <Receipt />
       <div className="border-t border-gray-500 mt-2">
         <p className="w-2/3 mx-auto p-2 text-sm">
           Thank you for choosing Picante. We hope you enjoy your meal.
         </p>
-        <p className="w-2/3 mx-auto p-2 text-sm">
-          MicroWave for 10 seconds, for delivery orders
-        </p>
+        <p className="w-2/3 mx-auto p-2 text-sm">Phone: +92 322 729 2020</p>
       </div>
     </div>
   );
