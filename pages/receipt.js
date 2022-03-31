f93994467a221fa97bbb0b2a9900f649b5de0dc8
Alwaz18 <alwazk8@gmail.com>
2022-03-31 19:29:17 +0500

    final

--- pages/receipt.js
+++ pages/receipt.js
@@ -4,9 +4,17 @@ import Image from "next/image";
 
 const receipt = () => {
   return (
-    <div className="flex flex-col justify-start bg-white mx-auto items-center ">
-      <Image src="/logo.jpeg " className="m-1" height={100} width={100} />
+    <div className="flex flex-col justify-start py-2 bg-white mx-auto items-center ">
+      <h1 className="text-2xl italic pt-1 pb-2"> PICANTE PK</h1>
       <Receipt />
+      <div className="border-t border-gray-500 mt-2">
+        <p className="w-2/3 mx-auto p-2 text-sm">
+          Thank you for choosing Picante. We hope you enjoy your meal.
+        </p>
+        <p className="w-2/3 mx-auto p-2 text-sm">
+          MicroWave for 10 seconds, for delivery orders
+        </p>
+      </div>
     </div>
   );
 };
