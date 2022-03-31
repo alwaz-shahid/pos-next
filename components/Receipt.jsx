842535056a2723c08d0055a3630640620d62bf50
Alwaz18 <alwazk8@gmail.com>
2022-03-31 18:35:32 +0500

    v02

--- components/Receipt.jsx
+++ components/Receipt.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import { grandTotal } from "../lib/billUtils";
 import useStore from "../store";
+import dayjs from "dayjs";
 
 const Receipt = () => {
+  let now = dayjs();
+  const nowTime = now.format("h:mm a");
+  const nowDate = now.format("MMMM D YYYY");
   const orders = useStore((state) => state.orders);
   const resetCart = useStore((state) => state.resetCart);
 
@@ -11,16 +15,16 @@ const Receipt = () => {
   let final_p = grandTotal(storeReceipt);
   useEffect(() => {}, [orders, resetCart, addOrder, final_p]);
   return (
-    <div className="w-80 max-w-max min-w-fit overflow-hidden f-col">
+    <div className="w-80 font-normal max-w-max min-w-fit overflow-hidden f-col">
       <table className="mx-auto table-auto text-sm">
         <thead>
           <tr className="font-normal">
             {/* <th className="m-2 underline">PID |</th> */}
-            <th className="m-2 px-2 underline"> Name </th>
-            <th className="m-2 px-2 underline">Price (PKR)</th>
-            <th className="m-2 px-2 underline">Qty </th>
-            <th className="m-2 px-2 underline">Discount (15%)</th>
-            <th className="m-2 px-2 underline">Final </th>
+            <th className="m-2 px-2"> Name </th>
+            <th className="m-2 px-2">Price (PKR)</th>
+            <th className="m-2 px-2">Qty </th>
+            <th className="m-2 px-2">Discount (15%)</th>
+            <th className="m-2 px-2">Final </th>
           </tr>
         </thead>
         <tbody>
@@ -36,10 +40,19 @@ const Receipt = () => {
           ))}
         </tbody>
       </table>
-      <div className="font-semibold p-1 min-w-full">
+      <div className=" p-1 min-w-full font-light text-sm">
         <div className="flex justify-between border-t border-b p-1 border-black">
-          <p>Total:</p>
-          <p>{final_p}</p>
+          <p>Grand Total :</p>
+          <p className="font-semibold">RS: {final_p}</p>
+        </div>
+        <hr />
+        <div className="flex justify-between ">
+          <p>Date :</p>
+          <p>{nowDate}</p>
+        </div>
+        <div className="flex justify-between ">
+          <p>Time :</p>
+          <p>{nowTime}</p>
         </div>
       </div>
     </div>
